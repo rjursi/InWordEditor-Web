bb25bbc2d875274c1fe7a42fe3925461b1521f85
YunKwon Kim <dfg3790@gmail.com>
2020-05-29 16:11:05 +0900

    Excel WordData Call

--- editor.aspx
+++ editor.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="editor.aspx.cs" Inherits="editor" %>
 
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -18,6 +20,9 @@
 </head>
 <body> 
     <form id="form_editor" runat="server">
+       
+        
+        <asp:ScriptManager ID="scriptManager_editor" runat="server"></asp:ScriptManager>
         <div class="container-fluid p-0 overflow-hidden">
             <div class="row min-vh-100">
                 <div class="col-md-2 col-lg-2 text-light" style="background-color : #0B3861">
@@ -38,24 +43,27 @@
                 </div>
                 
                 <div class="col-md-10 col-lg-10 pl-0" style="background-color : #0B3861">
-                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">   
                         <div>
                             <span class="navbar-brand">기능</span>
-                            <asp:Button ID="btn_callExcelFile" runat="server" CssClass="btn btn-info mr-2" Text="불러온 엑셀파일 적용" OnClick="btn_callExcelFile_Click" />
-                            <asp:FileUpload ID="fileUpload_excelFile" runat="server"/>
+                            <asp:Button ID="btn_callExcelFile" runat="server" CssClass="btn btn-info mr-2" Text="불러온 엑셀파일 적용" OnClick="btn_callExcelFile_Click" />     
+                            <asp:FileUpload ID="fileUpload_excelFile" runat="server"/>     
+                            <asp:Label ID="lbl_uploadStatus" runat="server"></asp:Label>
                             
                         </div>
+                        <asp:Button ID="btn_checkWord" runat="server" CssClass="btn btn-danger" Text="단어 유효성 확인" Visible="false"/>
                         <asp:Button ID="btn_outputWord" runat="server" Text="word 파일로 추출" CssClass="btn btn-primary ml-2" />
                         
                     </nav>
-                    <div class="form-group" style="height : 85vh">
-                        <asp:TextBox ID="txtbox_editor" runat="server" TextMode="MultiLine" CssClass="form-control h-100 mt-3"></asp:TextBox>
+                    <div class="form-group bg-white" style="height : 85vh">
+                        <asp:TextBox ID="txtbox_editor" runat="server" TextMode="MultiLine" CssClass="form-control h-100 mt-3 bg-white"></asp:TextBox>
+                        <ajaxToolkit:HtmlEditorExtender ID="htmlEditorEx_editor" runat="server" EnableSanitization="true" TargetControlID="txtbox_editor" Enabled="true"></ajaxToolkit:HtmlEditorExtender>
                     </div>
                 </div>
-               
+                
             </div>
         </div>
-        
+       
     </form>
 </body>
 </html>
--- editor.aspx.cs
+++ editor.aspx.cs
@@ -9,11 +9,10 @@ using System.Web.UI.WebControls;
 public partial class editor : System.Web.UI.Page
 {
     bool isExcelFileLoaded = false;
-    string savePath, filepath, filename;
+    
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        
         if (!Page.IsPostBack)
         {
             if(!string.IsNullOrEmpty(Session["username"] as string))
@@ -45,40 +44,74 @@ public partial class editor : System.Web.UI.Page
 
     protected void btn_exit_Click(object sender, EventArgs e)
     {
-        
 
-        
         // 임시로 저장한 파일을 다 사용하였으니 지움
-        File.Delete(filepath);
+        if (fileUpload_excelFile.HasFile)
+        {
+            File.Delete(Session["filepath"].ToString());
+        }
+
+        Session.Remove("filepath");
+        Session.Remove("filename");
+        Session.Remove("savePath");
+
         Server.Transfer("index.aspx");
     }
 
     protected void btn_callExcelFile_Click(object sender, EventArgs e)
     {
         // 가상 경로 문자열을 정적 경로로 변경해 반환
-        savePath = Server.MapPath("~/ForCheckExcelFile/");
+        Session["savePath"] = Server.MapPath("~/ForCheckExcelFile/");
 
         
         if (fileUpload_excelFile.HasFile)
         {
 
-            filename = fileUpload_excelFile.FileName;
-            filepath = savePath + filename;
+            Session["filename"] = fileUpload_excelFile.FileName;
+            Session["filepath"] = Session["savePath"].ToString() + Session["filename"].ToString();
 
             // 만약에 똑같은 파일명이 있으면 
-            fileUpload_excelFile.SaveAs(filepath);
+            fileUpload_excelFile.SaveAs(Session["filepath"].ToString());
 
             
             fileUpload_excelFile.Visible = false;
 
+            lbl_uploadStatus.Text = "지정한 파일이 업로드 되었습니다.";
+            lbl_uploadStatus.CssClass = "text-success ml-2";
+            btn_checkWord.Visible = true;
+            //callAlert(sender, "지정한 파일이 업로드 되었습니다.");
+
+            Session["workbookObj"] = new WorkbookSetting();
+                (Session["workbookObj"] as WorkbookSetting).selectExcelFile(Session["filepath"].ToString());
+                (Session["workbookObj"] as WorkbookSetting).selectWorksheet();
+
+            Session["worksheetReaderObj"] = new WorksheetReader(Session["workbookObj"] as WorkbookSetting);
+            Session["wordDataInfo"] = (Session["worksheetReaderObj"] as WorksheetReader).getWordDataInfo();
+
+            Session["words"] = (Session["wordDataInfo"] as DataInfo).getWords();
+
+            var wordListString = String.Join(",", Session["words"] as List<string>);
+
+            HttpCookie wordListCookie = new HttpCookie("wordList", wordListString);
+            wordListCookie.Expires = DateTime.Now.AddDays(7);
+            Response.Cookies.Add(wordListCookie);
+            
         }
         else
         {
-            
-            
+            lbl_uploadStatus.Text = "파일을 먼저 업로드 해주세요.";
+            lbl_uploadStatus.CssClass = "text-danger ml-2";
+            //callAlert(sender, "파일을 먼저 업로드 해주세요.");
         }
     }
 
+    /*
+    void callAlert(object sender, string message)
+    {
+        string alertMsg = "alert('" + message + "');";
+        ScriptManager.RegisterClientScriptBlock((sender as Control), this.GetType(), "alert", alertMsg, true);
+    }
+    */
 
   
 }
